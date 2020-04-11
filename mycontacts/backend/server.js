989f0f89d3e9c85ca6feb925afafad29218114b1
morenadam <adam.moren@hotmail.com>
2020-04-11 14:41:35 +0200

    Frontend backend setup

---
+++ mycontacts/backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+//Environment variable
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+//Hello world from the server!
+app.use("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
---
+++ mycontacts/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//Render the React app in root
+ReactDOM.render(<App />, document.getElementById('root'));
