6e8eee27e9649219d0263b0d5edc3cd5f6c0d236
Adam Moren <adam.moren1@gmail.com>
2020-04-13 21:51:35 +0200

    Added delete and update requests for backend

--- mycontacts/backend/routes/contacts.js
+++ mycontacts/backend/routes/contacts.js
@@ -1,29 +1,57 @@
-const router = require('express').Router();
-let Contact = require('../models/contacts.model');
+const router = require("express").Router();
+let Contact = require("../models/contacts.model");
 
 //Get request for getting all cotnacts
-router.route('/').get((req, res) => {
-    //Use the Contact model to find all the contacts that are in the DB
-    Contact.find()
-        .then(contacts => res.json(contacts))
-        .catch(err => res.status(400).json('Error: ' + err));
+router.route("/").get((req, res) => {
+  //Use the Contact model to find all the contacts that are in the DB
+  Contact.find()
+    .then((contacts) => res.json(contacts))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 //Post request for adding a new contact
-router.route('/add').post((req, res) => {
-    const name = req.body.name;
-    const address = req.body.address;
-    const phoneNumber = Number(req.body.phoneNumber);
+router.route("/add").post((req, res) => {
+  const name = req.body.name;
+  const address = req.body.address;
+  const phoneNumber = Number(req.body.phoneNumber);
 
-    const newContact = new Contact({
-        name,
-        address,
-        phoneNumber,
-    });
+  const newContact = new Contact({
+    name,
+    address,
+    phoneNumber,
+  });
 
-    newContact.save()
-        .then(() => res.json('Contact added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+  newContact
+    .save()
+    .then(() => res.json("Contact added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").get((req, res) => {
+  Contact.findById(req.params.id)
+    .then((Contact) => res.json(Contact))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Contact.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Contact deleted."))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Contact.findById(req.params.id)
+    .then((contact) => {
+      contact.name = req.body.name;
+      contact.address = req.body.address;
+      contact.phoneNumber = Number(req.body.phoneNumber);
+
+      contact
+        .save()
+        .then(() => res.json("Contact updated!"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 //Export the router
--- mycontacts/backend/server.js
+++ mycontacts/backend/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 
-const cors = require('cors');
-const mongoose = require('mongoose');
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 //Environment variable
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,22 +13,25 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-);
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-})
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 //Hello world from the server!
 // app.use("/", (req, res) => {
-//     res.send("Hello from the server!");
+//   res.send("Hello from the server!");
 // });
 
-const contactsRouter = require('./routes/contacts');
-app.use('/contacts', contactsRouter);
+const contactsRouter = require("./routes/contacts");
+app.use("/contacts", contactsRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
