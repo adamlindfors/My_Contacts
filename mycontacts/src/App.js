b6dc57146037f628bab9be1f6a52667eeb7794aa
Adam Moren <adam.moren1@gmail.com>
2020-04-15 19:47:34 +0200

    simple frontend for ContactList and New Contact

--- mycontacts/src/App.js
+++ mycontacts/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar";
+import ContactList from "./components/contactlist";
+import EditContact from "./components/edit-contact";
+import CreateContact from "./components/create-contact";
 
 function App() {
-    return (
-        <div>
-            <p>Hello from client !</p>
-        </div>
-    );
+  return (
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ContactList} />
+        <Route path="/edit/:id" component={EditContact} />
+        <Route path="/create" component={CreateContact} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
