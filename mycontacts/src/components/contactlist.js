900b86d20055daf70d62c3df7ed0c920ab21842d
Adam Moren <adam.moren1@gmail.com>
2020-04-16 10:49:49 +0200

    edit and delete contacts from frontend

--- mycontacts/src/components/contactlist.js
+++ mycontacts/src/components/contactlist.js
@@ -1,12 +1,89 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default class EditExercises extends Component {
+//Contact component. Maybe put in its own file.
+//Props = contact and deleteContact
+//key = contactID
+const Contact = (props) => (
+  <tr>
+    <td>{props.contact.name}</td>
+    <td>{props.contact.address}</td>
+    <td>{props.contact.phoneNumber}</td>
+    <td>
+      <Link to={"/edit/" + props.contact._id}>edit</Link> |{" "}
+      {/* Should be a real button */}
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteContact(props.contact._id);
+        }}
+      >
+        delete
+      </a>
+    </td>
+  </tr>
+);
+
+class ContactList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { contacts: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/contacts/")
+      .then((response) => {
+        this.setState({ contacts: response.data });
+        //test
+        console.log(this.state.contacts);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  deleteContact = (id) => {
+    axios.delete("http://localhost:5000/contacts/" + id).then((response) => {
+      console.log(response.data);
+    });
+
+    this.setState({
+      contacts: this.state.contacts.filter((del) => del._id !== id),
+    });
+  };
+
+  contactList = () => {
+    return this.state.contacts.map((currentContact) => {
+      return (
+        <Contact
+          contact={currentContact}
+          deleteContact={this.deleteContact}
+          key={currentContact._id}
+        />
+      );
+    });
+  };
+
   render() {
     return (
       <div>
-        <p>ContactList</p>
+        <h3>Contacts</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>name</th>
+              <th>Address</th>
+              <th>Phone number</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.contactList()}</tbody>
+        </table>
       </div>
     );
   }
 }
+
+export default ContactList;
--- mycontacts/src/components/edit-contact.js
+++ mycontacts/src/components/edit-contact.js
@@ -1,12 +1,114 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default class EditExercises extends Component {
+class EditContact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      address: "",
+      phoneNumber: 0,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/contacts/" + this.props.match.params.id)
+      .then((response) => {
+        this.setState({
+          name: response.data.name,
+          address: response.data.address,
+          phoneNumber: response.data.phoneNumber,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  onChangeName = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+
+  onChangeAddress = (e) => {
+    this.setState({
+      address: e.target.value,
+    });
+  };
+
+  onChangePhoneNumber = (e) => {
+    this.setState({
+      phoneNumber: e.target.value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const contact = {
+      name: this.state.name,
+      address: this.state.address,
+      phoneNumber: this.state.phoneNumber,
+    };
+
+    axios
+      .post(
+        "http://localhost:5000/contacts/update/" + this.props.match.params.id,
+        contact
+      )
+      .then((res) => console.log(res.data));
+
+    window.location = "/";
+  };
+
   render() {
     return (
       <div>
-        <p>edit ContactList</p>
+        <h3>Edit contact</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Name: </label>
+            <input
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.name}
+              onChange={this.onChangeName}
+            ></input>
+          </div>
+          <div className="form-group">
+            <label>Address: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.address}
+              onChange={this.onChangeAddress}
+            />
+          </div>
+          <div className="form-group">
+            <label>Telephone Number: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.phoneNumber}
+              onChange={this.onChangePhoneNumber}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Save changes"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
       </div>
     );
   }
 }
+
+export default EditContact;
