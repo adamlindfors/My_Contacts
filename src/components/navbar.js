fd5765aa190abba159026e00a085b9a2c98e1157
Adam Moren <adam.moren1@gmail.com>
2020-05-03 21:32:48 +0200

    Updated Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,23 +1,82 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { withAuth } from "@okta/okta-react";
+import { connect } from "react-redux";
+import { logoutContacts } from "../actions/contactActions";
+import { userLogin, userLogout } from "../actions/authActions";
+import PropTypes from "prop-types";
 
-export default class Navbar extends Component {
+class Navbar extends Component {
+  login = async () => {
+    this.props.auth.login("/");
+  };
+
+  logout = async () => {
+    this.props.userLogout();
+    this.props.logoutContacts();
+    await this.props.auth.logout("/");
+  };
+
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
         <Link to="/" className="navbar-brand">
-          MyContacts
+          My Contacts
         </Link>
         <div className="collpase navbar-collapse">
           <ul className="navbar-nav mr-auto">
             <li className="navbar-item">
-              <Link to="/create" className="nav-link">
-                Create New Contact
-              </Link>
+              {this.props.authReducer.subID ? (
+                <Link to="/create" className="nav-link">
+                  Create New Contact
+                </Link>
+              ) : (
+                ""
+              )}
             </li>
           </ul>
+          {this.props.authReducer.subID ? (
+            <button className="btn btn-dark my-2 my-sm-0" onClick={this.logout}>
+              Logout
+            </button>
+          ) : (
+            <div>
+              <button
+                className="btn btn-dark my-2 my-sm-0"
+                onClick={this.login}
+              >
+                Login
+              </button>
+              <button
+                className="btn btn-dark my-2 my-sm-0"
+                onClick={this.login}
+              >
+                Sign Up
+              </button>
+            </div>
+          )}
         </div>
       </nav>
     );
   }
 }
+
+Navbar.propTypes = {
+  logoutContacts: PropTypes.func.isRequired,
+  contactReducer: PropTypes.object.isRequired,
+  authReducer: PropTypes.object.isRequired,
+  userLogin: PropTypes.func.isRequired,
+  userLogout: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  authReducer: state.authReducer,
+  contactReducer: state.contactReducer,
+});
+
+//Connect component to the store
+export default connect(mapStateToProps, {
+  userLogin,
+  userLogout,
+  logoutContacts,
+})(withAuth(Navbar));
